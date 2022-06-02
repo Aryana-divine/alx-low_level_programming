fb48ff2ff62850dc116c95a7020f1cf390312609
Aryana-divine <divinearyana0@gmail.com>
2022-06-02 16:26:13 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,17 @@
 
 
 
-/*
- *  *  main - prints the phrase "Programming is like building a multilingual
+/**
+ *  * main - Entry point
  *
- *  *  puzle"
+ *  *
  *
- *  *  Return: 0 if exited properly, non-zero otherwise
+ *  * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
-
+	return (0);
 }
