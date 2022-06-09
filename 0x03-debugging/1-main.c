00dd7b79c109b204814bbca0008d5d3c740a7c53
Aryana-divine <divinearyana0@gmail.com>
2022-06-09 14:21:04 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  *  * main - causes an infinite loop
  *
  *  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -18,4 +20,5 @@ int main(void)
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
+
 }
