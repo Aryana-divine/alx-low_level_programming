8bd2e69b95149772bd2ab074530ee2194c1baa81
Aryana-divine <divinearyana0@gmail.com>
2022-06-13 13:02:23 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char *guard = dest;
 
-	for (; src[i] != '\0'; i++)
+	while (*src)
 	{
-	dest[i] = src[i];
+	*dest++ = *src++;
+	*dest = 0;
 	}
-	return (dest);
+
+	return (guard);
 
 }
