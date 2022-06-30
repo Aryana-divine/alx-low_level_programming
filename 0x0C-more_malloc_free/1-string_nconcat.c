8c8732c25211f85cf234c41756b41271807d802d
Aryana-divine <divinearyana0@gmail.com>
2022-06-30 07:44:28 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,27 +32,28 @@ int _strlen(char *string)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	int num, len, i, j;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	num = n;
-	if (s1 == NULL) /* account for NULL strings */
-	s1 = "";
-	if (s2 == NULL)
-	s2 = "";
-	if (num < 0) /* account for negative n bytes */
+	while (s1 && s1[len1])
+	len1++;
+	while (s2 && s2[len2])
+	len2++;
+	if (n < len2)
+	s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 	return (NULL);
-	if (num >= _strlen(s2)) /* account for n too big */
-	num = _strlen(s2);
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-	if (ptr == NULL)
-	return (NULL);
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
-	ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-	ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
-	return (ptr);
-
+	while (i < len1)
+	{
+	s[i] = s1[i];
+	i++;
+	}
+	while (n < len2 && i < (len1 + n))
+	s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+	s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
